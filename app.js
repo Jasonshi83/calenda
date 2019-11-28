c36f6bf476914e7961af5cba082a88d4291f86d7
Jason <shizong@outlook.com>
2019-11-28 23:38:35 +1300

    Add columns on the page

--- app.js
+++ app.js
@@ -20,3 +20,53 @@ var calendar = new tui.Calendar("#calendar", {
         // ...
     }
 });
+
+
+
+const refreshImage = function () {
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (myJson) {
+            console.log(myJson)
+            $("#demo-img").attr("src", myJson.message);
+        });
+}
+
+$('#refresh-me').click(refreshImage);
+
+
+// calendar.createSchedules([{
+//         id: '1',
+//         calendarId: '1',
+//         title: 'my schedule',
+//         category: 'time',
+//         dueDateClass: '',
+//         start: '2018-01-18T22:30:00+09:00',
+//         end: '2018-01-19T02:30:00+09:00'
+//     },
+//     {
+//         id: '2',
+//         calendarId: '1',
+//         title: 'second schedule',
+//         category: 'time',
+//         dueDateClass: '',
+//         start: '2018-01-18T17:30:00+09:00',
+//         end: '2018-01-19T17:31:00+09:00',
+//         isReadOnly: true // schedule is read-only
+//     }
+// ]);
+
+//Data picker----
+var container = document.getElementById('tui-date-picker-container');
+var target = document.getElementById('tui-date-picker-target');
+
+
+var instance = new tui.DatePicker(container, {
+    input: {
+        element: target
+    },
+});
+instance.getDate();
+//--------
